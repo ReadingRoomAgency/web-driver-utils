c6e2d90ae6f1ddc279d73b0d588f8c40fc3aef8f
david.didiego <david.didiego@readingroom.com>
2017-10-24 15:49:54 +0100

    web-driver-utils add new helpers to the index

--- src/index.js
+++ src/index.js
@@ -1,57 +1,62 @@
-const checkFlags = require('./checkFlags');
-const clickOnElement = require('./clickOnElement');
-const constants = require('./constants');
-const dataTableFirstRow = require('./dataTableFirstRow');
-const dataTableRowsHash = require('./dataTableRowsHash');
-const delay = require('./delay');
-const eachElementHasCssValue = require('./eachElementHasCssValue');
-const eachElementHasElement = require('./eachElementHasElement');
-const elementAttributeIs = require('./elementAttributeIs');
-const elementAttributeMatchesExpression = require('./elementAttributeMatchesExpression');
-const elementComputedStyleContains = require('./elementComputedStyleContains');
-const elementDoeNotHasClass = require('./elementDoeNotHasClass');
-const elementDoesNotExist = require('./elementDoesNotExist');
-const elementHasClass = require('./elementHasClass');
-const elementHasCssValue = require('./elementHasCssValue');
-const elementInlineStyleIs = require('./elementInlineStyleIs');
-const elementInlineStyleMatchesExpression = require('./elementInlineStyleMatchesExpression');
-const elementIsLocated = require('./elementIsLocated');
-const elementIsNotVisible = require('./elementIsNotVisible');
-const elementIsVisible = require('./elementIsVisible');
-const elementsCountIs = require('./elementsCountIs');
-const elementsCountMatchesExpression = require('./elementsCountMatchesExpression');
-const elementTextIs = require('./elementTextIs');
-const elementTextIsNotEmpty = require('./elementTextIsNotEmpty');
-const elementTextMatchesExpression = require('./elementTextMatchesExpression');
-const getElementInlineStyles = require('./getElementInlineStyles');
-const getElementsTextAsArray = require('./getElementsTextAsArray');
-const getElementStyles = require('./getElementStyles');
-const getNumericValue = require('./getNumericValue');
-const goToUrl = require('./goToUrl');
-const isImageLoaded = require('./isImageLoaded');
-const pageTitleIs = require('./pageTitleIs');
-const range = require('./range');
-const scrolledToElement = require('./scrolledToElement');
-const scrollToElement = require('./scrollToElement');
-const textIsInElementChildren = require('./textIsInElementChildren');
-const typeInElement = require('./typeInElement');
-const waitUntilElementIsNotLocated = require('./waitUntilElementIsNotLocated');
-const waitUntilElementIsNotVisible = require('./waitUntilElementIsNotVisible');
-const waitUntilElementIsVisible = require('./waitUntilElementIsVisible');
-const waitUntilElementsAreVisible = require('./waitUntilElementsAreVisible');
-const waitUntilElementVisible = require('./waitUntilElementVisible');
-const webElementAttributeIncludes = require('./webElementAttributeIncludes');
-const webElementAttributeIs = require('./webElementAttributeIs');
-const webElementInlineStyleIs = require('./webElementInlineStyleIs');
-const webElementIsImageLoaded = require('./webElementIsImageLoaded');
-const webElementTextIs = require('./webElementTextIs');
-const webElementTextIsInChildren = require('./webElementTextIsInChildren');
-const webElementTextIsNotEmpty = require('./webElementTextIsNotEmpty');
+'use strict';
 
+var checkFlags = require('./checkFlags');
+var clickOnElement = require('./clickOnElement');
+var clickOnParentElement = require('./clickOnParentElement');
+var constants = require('./constants');
+var dataTableFirstRow = require('./dataTableFirstRow');
+var dataTableRowsHash = require('./dataTableRowsHash');
+var delay = require('./delay');
+var eachElementHasCssValue = require('./eachElementHasCssValue');
+var eachElementHasElement = require('./eachElementHasElement');
+var elementAttributeIs = require('./elementAttributeIs');
+var elementAttributeMatchesExpression = require('./elementAttributeMatchesExpression');
+var elementComputedStyleContains = require('./elementComputedStyleContains');
+var elementDoeNotHasClass = require('./elementDoeNotHasClass');
+var elementDoesNotExist = require('./elementDoesNotExist');
+var elementHasClass = require('./elementHasClass');
+var elementHasCssValue = require('./elementHasCssValue');
+var elementInlineStyleIs = require('./elementInlineStyleIs');
+var elementInlineStyleMatchesExpression = require('./elementInlineStyleMatchesExpression');
+var elementIsLocated = require('./elementIsLocated');
+var elementIsNotVisible = require('./elementIsNotVisible');
+var elementIsVisible = require('./elementIsVisible');
+var elementsCountIs = require('./elementsCountIs');
+var elementsCountMatchesExpression = require('./elementsCountMatchesExpression');
+var elementTextIs = require('./elementTextIs');
+var elementTextIsNotEmpty = require('./elementTextIsNotEmpty');
+var focusOutOfElement = require('./focusOutOfElement');
+var elementTextMatchesExpression = require('./elementTextMatchesExpression');
+var getElementInlineStyles = require('./getElementInlineStyles');
+var getElementsTextAsArray = require('./getElementsTextAsArray');
+var getElementStyles = require('./getElementStyles');
+var getNumericValue = require('./getNumericValue');
+var goToUrl = require('./goToUrl');
+var isImageLoaded = require('./isImageLoaded');
+var pageTitleIs = require('./pageTitleIs');
+var range = require('./range');
+var resizeBrowser = require('./resizeBrowser');
+var scrolledToElement = require('./scrolledToElement');
+var scrollToElement = require('./scrollToElement');
+var textIsInElementChildren = require('./textIsInElementChildren');
+var typeInElement = require('./typeInElement');
+var waitUntilElementIsNotLocated = require('./waitUntilElementIsNotLocated');
+var waitUntilElementIsNotVisible = require('./waitUntilElementIsNotVisible');
+var waitUntilElementIsVisible = require('./waitUntilElementIsVisible');
+var waitUntilElementsAreVisible = require('./waitUntilElementsAreVisible');
+var waitUntilElementVisible = require('./waitUntilElementVisible');
+var webElementAttributeIncludes = require('./webElementAttributeIncludes');
+var webElementAttributeIs = require('./webElementAttributeIs');
+var webElementInlineStyleIs = require('./webElementInlineStyleIs');
+var webElementIsImageLoaded = require('./webElementIsImageLoaded');
+var webElementTextIs = require('./webElementTextIs');
+var webElementTextIsInChildren = require('./webElementTextIsInChildren');
+var webElementTextIsNotEmpty = require('./webElementTextIsNotEmpty');
 
 module.exports = {
   checkFlags: checkFlags,
   clickOnElement: clickOnElement,
+  clickOnParentElement: clickOnParentElement,
   constants: constants,
   dataTableFirstRow: dataTableFirstRow,
   dataTableRowsHash: dataTableRowsHash,
@@ -75,6 +80,7 @@ module.exports = {
   elementTextIs: elementTextIs,
   elementTextIsNotEmpty: elementTextIsNotEmpty,
   elementTextMatchesExpression: elementTextMatchesExpression,
+  focusOutOfElement: focusOutOfElement,
   getElementInlineStyles: getElementInlineStyles,
   getElementsTextAsArray: getElementsTextAsArray,
   getElementStyles: getElementStyles,
@@ -82,6 +88,7 @@ module.exports = {
   goToUrl: goToUrl,
   isImageLoaded: isImageLoaded,
   pageTitleIs: pageTitleIs,
+  resizeBrowser: resizeBrowser,
   range: range,
   scrolledToElement: scrolledToElement,
   scrollToElement: scrollToElement,
@@ -98,5 +105,5 @@ module.exports = {
   webElementIsImageLoaded: webElementIsImageLoaded,
   webElementTextIs: webElementTextIs,
   webElementTextIsInChildren: webElementTextIsInChildren,
-  webElementTextIsNotEmpty: webElementTextIsNotEmpty,
-}
+  webElementTextIsNotEmpty: webElementTextIsNotEmpty
+};
